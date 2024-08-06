01e44054501a0c5b5d9e5017600074f2b3c2f90e
Kacper <kacper.gajko1@gmail.com>
2024-08-06 22:58:11 +0200

    imported 3d model loader

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 import * as THREE from 'three'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
+
+const loader = new GLTFLoader();
 
 const scene =  new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000)
 
+
+
+//Renderer
 const renderer =  new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement)
