24f9878c24d5eb1d8bb9d25d74bd3427a2a418b3
Kacper <kacper.gajko1@gmail.com>
2024-08-07 00:05:02 +0200

    added orbit controls

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
+import { OrbitControls } from 'three/addons/controls/OrbitControls'
 
 let model, loader, scene, camera, renderer;
 
@@ -14,6 +15,18 @@ scene.background = new THREE.Color(0xbfe3dd)
 
 //camera
 camera.position.set(0, 0, 10)
+
+//Renderer
+renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement)
+
+//controls
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.target.set(0, 0.5, 0)
+controls.update()
+controls.enablePan = false
+controls.enambleDamping = true;
 
 //Loading Heart
 loader.load("/heart.glb", function(gltf) {
@@ -32,12 +45,11 @@ loader.load("/heart.glb", function(gltf) {
 const ambientLight = new THREE.AmbientLight(0x404040);
 scene.add(ambientLight);
 
-//Renderer
-renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement)
 
 function renderScene() {
+
+  controls.update()
+
   renderer.render(scene, camera)
 }
 
