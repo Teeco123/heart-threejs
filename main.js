24b9ef496e4f54e29abc204ae833244ddbde4844
Kacper <kacper.gajko1@gmail.com>
2024-08-07 00:36:52 +0200

    background is black instead of dots

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let model
 
 //Scene
 const scene = new THREE.Scene()
-scene.background = new THREE.Color(0xbfe3dd)
+scene.background = new THREE.Color(255, 255, 255)
 
 //camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
