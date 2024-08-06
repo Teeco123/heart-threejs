a7ef4b64c7b8f737a5b05d1afd81f4f0572896aa
Kacper <kacper.gajko1@gmail.com>
2024-08-06 23:37:22 +0200

    properly displaying heart

--- main.js
+++ main.js
@@ -6,8 +6,11 @@ const loader = new GLTFLoader();
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
+//Scene
+scene.background = new THREE.Color(0xbfe3dd)
+
 //camera
-camera.position.z = 5
+camera.position.set(0, 0, 10)
 
 //Loading Heart
 loader.load("/heart.glb", function(gltf) {
@@ -15,6 +18,9 @@ loader.load("/heart.glb", function(gltf) {
 
   model.position.set(0, 0, 0)
   model.scale.set(1, 1, 1)
+
+  scene.add(model)
+
 }, undefined, function(error) {
   console.error(error);
 })
