5f2b0c7f38129b77b1fa1ca4ae0c395cef7ef36a
Kacper <kacper.gajko1@gmail.com>
2024-08-06 23:05:32 +0200

    imported heart model

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
 
 const loader = new GLTFLoader();
 
-const scene =  new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000)
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
+loader.load("/heart.gltf", function(gltf) {
+  scene.add(gltf.scene);
+}, undefined, function(error) {
+  console.error(error);
+})
 
 
 //Renderer
-const renderer =  new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild( renderer.domElement)
+document.body.appendChild(renderer.domElement)
